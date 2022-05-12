1ec076b667d70fde7d92eef8db21a205abd4147a
chaeyoungshim <cyshim7@gmail.com>
2022-05-12 18:02:37 +0900

    board + security

---
+++ security_javaconfig/src/main/java/com/study/config/RootConfig.java
@@ -0,0 +1,51 @@
+package com.study.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@EnableTransactionManagement
+@EnableScheduling	 //<tx:annotation-driven/>
+@MapperScan("com.study.mapper")
+@ComponentScan({"com.study.service","com.study.task","com.study.handler"}) //스캔해줘라
+@Configuration
+public class RootConfig {
+	
+	@Bean	//객체 생성하려면 이 어노테이션 써야 함
+	public DataSource dataSource() {
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setDriverClassName("oracle.jdbc.OracleDriver");
+		hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:xe");
+		hikariConfig.setUsername("javadb");
+		hikariConfig.setPassword("12345");
+
+		HikariDataSource datasource = new HikariDataSource(hikariConfig);
+		return datasource;
+		
+	}
+	
+	@Bean
+	public SqlSessionFactory sqlSessionFactory() throws Exception {
+		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+		sqlSessionFactoryBean.setDataSource(dataSource());
+		return sqlSessionFactoryBean.getObject();
+	}
+	
+	@Bean
+	public DataSourceTransactionManager txManager() {
+		return new DataSourceTransactionManager(dataSource());
+	}
+	
+	
+}
